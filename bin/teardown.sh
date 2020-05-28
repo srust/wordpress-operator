d056f24f7be437ca442d5d0c988793940ab0f533
Stephen Rust <srust@srust.org>
2020-05-27 23:21:23 -0400

    teardown update

--- bin/teardown.sh
+++ bin/teardown.sh
@@ -1,5 +1,7 @@
+kubectl delete -f wordpress.yaml
+kubectl delete -f deploy/operator.yaml
 kubectl delete -f deploy/role.yaml
 kubectl delete -f deploy/role_binding.yaml
 kubectl delete -f deploy/service_account.yaml
-kubectl delete -f deploy/operator.yaml
-kubectl delete -f wordpress.yaml
+kubectl delete pvc/wp-pv-claim
+kubectl delete pvc/mysql-pv-claim
