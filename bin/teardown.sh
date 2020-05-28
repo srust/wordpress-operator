8134e2c94deb41368f1cc1cdf6b34f4ef8d795a9
Stephen Rust <srust@srust.org>
2020-05-27 23:02:02 -0400

    add helper scripts for testing

---
+++ bin/teardown.sh
@@ -0,0 +1,5 @@
+kubectl delete -f deploy/role.yaml
+kubectl delete -f deploy/role_binding.yaml
+kubectl delete -f deploy/service_account.yaml
+kubectl delete -f deploy/operator.yaml
+kubectl delete -f wordpress.yaml
